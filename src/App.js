32068f1ffcb174f10e1eeac180311830b989c254
vishalamc <vishalamc@gmail.com>
2025-07-20 22:17:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Page1 from "./pages/page1";
-import Page2 from "./pages/page2";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
+import Introduction from "./pages/Introduction";
+import Variables from "./pages/Variables";
+import Loops from "./pages/Loops";
+import Identifier from "./pages/Identifier";
 
 function App() {
   return (
     <Router>
-      <div style={{ padding: 20 }}>
-        <nav>
-          <Link to="/page1" style={{ margin: 10 }}>Page 1</Link>
-          <Link to="/page2" style={{ margin: 10 }}>Page 2</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} /> {/* This loads first */}
+        <Route path="/home" element={<Home />}>
+          <Route path="introduction" element={<Introduction />} />
+          <Route path="variables" element={<Variables />} />
+          <Route path="loops" element={<Loops />} />
+          <Route path="identifier" element={<Identifier />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
