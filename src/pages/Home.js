7031df7c0e2bb9d436ec238ad81b53e31eed4ee5
vishalamc <vishalamc@gmail.com>
2025-07-22 15:13:03 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,51 @@
-// pages/Home.js
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Button, List, ListItem, ListItemText, Divider, Typography } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="container">
       <div className="sidebar">
-         <Button
+        <Button
           variant="contained"
           startIcon={<HomeIcon />}
           component={Link}
           to="/"
-          style={{ marginBottom: '20px' }}
+          sx={{
+            backgroundColor: '#1976d2',
+            color: 'white',
+            marginBottom: '20px',
+            width: '100%',
+            textTransform: 'none'
+          }}
         >
-    Main Page
+          Main Page
         </Button>
-        <h2 style={{ color: 'blue' }}>C Programming Topics</h2>
 
-       
+        <Typography variant="h6" gutterBottom color="white">
+          <MenuBookIcon fontSize="small" sx={{ mr: 1 }} />
+          C Programming Topics
+        </Typography>
 
-        <ul>
-          <li><strong style={{ color: 'purple' }}>Fundamentals</strong>
-            <ul>
-              <li><Link to="introduction">Introduction</Link></li>
-              <li><Link to="identifier">Identifier</Link></li>
-              <li><Link to="keyword">Keywords</Link></li>
-              <li><Link to="datatypes">Datatypes</Link></li>
-              <li><Link to="variables">Variables</Link></li>
-              <li><Link to="loops">Loops</Link></li>
-            
-            </ul>
-          </li>
-        </ul>
+        <Divider sx={{ borderColor: 'rgba(255,255,255,0.2)', mb: 1 }} />
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemText primary="Fundamentals" primaryTypographyProps={{ style: { color: '#ffd54f', fontWeight: 'bold' } }} />
+          </ListItem>
+          <ul className="sub-list">
+            <li><Link to="introduction">Introduction</Link></li>
+            <li><Link to="identifier">Identifier</Link></li>
+            <li><Link to="keyword">Keywords</Link></li>
+            <li><Link to="datatypes">Datatypes</Link></li>
+            <li><Link to="constant">Constant</Link></li>
+            <li><Link to="variables">Variables</Link></li>
+            <li><Link to="loops">Loops</Link></li>
+          </ul>
+        </List>
       </div>
 
       <div className="main-content">
