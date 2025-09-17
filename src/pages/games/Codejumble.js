519f1b8173b2a9bda13717d1464402c9d751e4f3
vishalamc <vishalamc@gmail.com>
2025-09-17 15:36:51 +0530

    Update Codejumble.js

--- src/pages/games/Codejumble.js
+++ src/pages/games/Codejumble.js
@@ -157,9 +157,6 @@ const CodeArrangeGame = () => {
         </Grid>
       </Container>
 
-      <Box component="footer" sx={{ p: 2, textAlign: 'center', bgcolor: 'primary.main', color: 'white' }}>
-        <Typography variant="body2">© 2025 GyanManthan Gurukul. All Rights Reserved.</Typography>
-      </Box>
     </DndProvider>
   );
 };
@@ -167,3 +164,4 @@ const CodeArrangeGame = () => {
 const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
 
 export default CodeArrangeGame;
+
